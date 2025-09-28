b42288b0a72305d6be42210a9730c457bbef0718
delirium6410 <libra@mailbox.org>
2025-09-28 21:34:24 +0200

    Testing

---
+++ modules/home-manager/vscode.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, lib, ... }:
+{
+  options = {
+    machine.vscode.enable = lib.mkEnableOption "";
+  };
+
+  config = lib.mkIf config.machine.vscode.enable {
+    programs.vscode = {
+      enable = true;
+      mutableExtensionsDir = false;
+      profiles.default = {
+        enableUpdateCheck = false;
+        enableExtensionUpdateCheck = false;
+        extensions = [];
+        userSettings = {};
+      };
+    };
+  };
+}
